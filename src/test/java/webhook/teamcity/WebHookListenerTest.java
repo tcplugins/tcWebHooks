0b9f54af8a5e01e8f262bae93cf7fb1744d6f2d7
netwolfuk <netwolfuk>
2009-10-30 23:43:24 +0000

    tcWebHooks - A few more tests
    git-svn-id: https://svn.code.sf.net/p/tcplugins/code/tcWebHooks/branches/0.8.x.x@76 1e73634f-3015-4432-a249-9c159a3a9991

--- src/test/java/webhook/teamcity/WebHookListenerTest.java
+++ src/test/java/webhook/teamcity/WebHookListenerTest.java
@@ -1,12 +1,9 @@
 package webhook.teamcity;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
-
 import jetbrains.buildServer.messages.Status;
 import jetbrains.buildServer.serverSide.SBuildServer;
 import jetbrains.buildServer.serverSide.SFinishedBuild;
-import jetbrains.buildServer.serverSide.SRunningBuild;
 import jetbrains.buildServer.serverSide.settings.ProjectSettingsManager;
 
 import org.junit.After;
@@ -52,10 +49,10 @@ public class WebHookListenerTest {
 		whl.register();
 	}
 
-	@Test
-	public void testGetFromConfig() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testGetFromConfig() {
+//		fail("Not yet implemented");
+//	}
 
 	@Test
 	public void testBuildStartedSRunningBuild() {
@@ -72,27 +69,61 @@ public class WebHookListenerTest {
 
 	@Test
 	public void testBuildFinishedSRunningBuild() {
-		fail("Not yet implemented");
+		MockSBuildType sBuildType = new MockSBuildType("Test Build", "A Test Build", "bt1");
+		String triggeredBy = "SubVersion";
+		MockSRunningBuild sRunningBuild = new MockSRunningBuild(sBuildType, triggeredBy, Status.NORMAL, "Running");
+		SFinishedBuild previousBuild = mock(SFinishedBuild.class);
+		MockSProject sProject = new MockSProject("Test Project", "A test project", "project1", sBuildType);
+		sBuildType.setProject(sProject);
+		WebHookListener whl = new WebHookListener(sBuildServer, settings,configSettings, manager);
+		whl.register();
+		whl.buildFinished(sRunningBuild);
 	}
 
 	@Test
 	public void testBuildInterruptedSRunningBuild() {
-		fail("Not yet implemented");
+		MockSBuildType sBuildType = new MockSBuildType("Test Build", "A Test Build", "bt1");
+		String triggeredBy = "SubVersion";
+		MockSRunningBuild sRunningBuild = new MockSRunningBuild(sBuildType, triggeredBy, Status.NORMAL, "Running");
+		SFinishedBuild previousBuild = mock(SFinishedBuild.class);
+		MockSProject sProject = new MockSProject("Test Project", "A test project", "project1", sBuildType);
+		sBuildType.setProject(sProject);
+		WebHookListener whl = new WebHookListener(sBuildServer, settings,configSettings, manager);
+		whl.register();
+		whl.buildInterrupted(sRunningBuild);
 	}
 
 	@Test
 	public void testBeforeBuildFinishSRunningBuild() {
-		fail("Not yet implemented");
+		MockSBuildType sBuildType = new MockSBuildType("Test Build", "A Test Build", "bt1");
+		String triggeredBy = "SubVersion";
+		MockSRunningBuild sRunningBuild = new MockSRunningBuild(sBuildType, triggeredBy, Status.NORMAL, "Running");
+		SFinishedBuild previousBuild = mock(SFinishedBuild.class);
+		MockSProject sProject = new MockSProject("Test Project", "A test project", "project1", sBuildType);
+		sBuildType.setProject(sProject);
+		WebHookListener whl = new WebHookListener(sBuildServer, settings,configSettings, manager);
+		whl.register();
+		whl.beforeBuildFinish(sRunningBuild);
 	}
 
 	@Test
 	public void testBuildChangedStatusSRunningBuildStatusStatus() {
-		fail("Not yet implemented");
+		MockSBuildType sBuildType = new MockSBuildType("Test Build", "A Test Build", "bt1");
+		String triggeredBy = "SubVersion";
+		MockSRunningBuild sRunningBuild = new MockSRunningBuild(sBuildType, triggeredBy, Status.NORMAL, "Running");
+		SFinishedBuild previousBuild = mock(SFinishedBuild.class);
+		MockSProject sProject = new MockSProject("Test Project", "A test project", "project1", sBuildType);
+		sBuildType.setProject(sProject);
+		WebHookListener whl = new WebHookListener(sBuildServer, settings,configSettings, manager);
+		Status oldStatus = Status.NORMAL;
+		Status newStatus = Status.FAILURE;
+		whl.register();
+		whl.buildChangedStatus(sRunningBuild, oldStatus, newStatus);
 	}
 
-	@Test
-	public void testResponsibleChangedSBuildTypeResponsibilityInfoResponsibilityInfoBoolean() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testResponsibleChangedSBuildTypeResponsibilityInfoResponsibilityInfoBoolean() {
+//		
+//	}
 
 }
