75e0bf435d74166259732f61963ad029a4595bab
netwolfuk <netwolfuk@gmail.com>
2015-10-09 23:14:53 +1300

    Don't return null object. We want to see changes even if they are empty.

--- tcwebhooks-core/src/main/java/webhook/teamcity/payload/content/WebHooksChanges.java
+++ tcwebhooks-core/src/main/java/webhook/teamcity/payload/content/WebHooksChanges.java
@@ -11,10 +11,6 @@ public class WebHooksChanges extends ArrayList<WebHooksChange>{
 	private static final long serialVersionUID = 1L;
 	
 	public static WebHooksChanges build (List<SVcsModification> mods){
-		if (mods.isEmpty()){
-			return null;
-		}
-		
 		WebHooksChanges changes = new WebHooksChanges();
 		
 		for (SVcsModification modification: mods){
