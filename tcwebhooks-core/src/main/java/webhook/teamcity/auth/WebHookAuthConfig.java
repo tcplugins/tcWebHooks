753ccbe8325c5da2c364047562345e8f1bc33f2f
netwolfuk <netwolfuk@users.noreply.github.com>
2016-10-14 09:22:42 +1300

    Add checking for required auth paramters
     - Shows red tab and error message if a require fields is empty.
     - Enhancements to issue #50

--- tcwebhooks-core/src/main/java/webhook/teamcity/auth/WebHookAuthConfig.java
+++ tcwebhooks-core/src/main/java/webhook/teamcity/auth/WebHookAuthConfig.java
@@ -3,8 +3,17 @@ package webhook.teamcity.auth;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Singular;
+
+@Setter @Builder @AllArgsConstructor @NoArgsConstructor
 public class WebHookAuthConfig {
 	public String type = "";
 	public Boolean preemptive = true;
+	
+	@Singular
 	public Map<String, String> parameters = new LinkedHashMap<>();
 }
