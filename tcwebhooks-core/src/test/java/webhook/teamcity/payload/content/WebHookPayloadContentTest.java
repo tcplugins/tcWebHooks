dde9695b32af68a637fabec065b94df0147032e2
Alexey Makhov <amakhov@avito.ru>
2017-02-10 22:06:45 +0300

    fix tests

--- tcwebhooks-core/src/test/java/webhook/teamcity/payload/content/WebHookPayloadContentTest.java
+++ tcwebhooks-core/src/test/java/webhook/teamcity/payload/content/WebHookPayloadContentTest.java
@@ -1,19 +1,18 @@
 package webhook.teamcity.payload.content;
 
-import static org.junit.Assert.*;
+import org.jdom.JDOMException;
+import org.junit.Before;
+import org.junit.Test;
+import webhook.teamcity.BuildStateEnum;
+import webhook.testframework.WebHookMockingFramework;
+import webhook.testframework.WebHookMockingFrameworkImpl;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import org.jdom.JDOMException;
-import org.junit.Before;
-import org.junit.Test;
-
-import webhook.teamcity.BuildStateEnum;
-import webhook.testframework.WebHookMockingFramework;
-import webhook.testframework.WebHookMockingFrameworkImpl;
+import static org.junit.Assert.assertTrue;
 
 public class WebHookPayloadContentTest {
 	
@@ -31,7 +30,7 @@ public class WebHookPayloadContentTest {
 		framework = WebHookMockingFrameworkImpl.create(BuildStateEnum.BUILD_FINISHED, extraParameters, teamcityProperties);
 		framework.loadWebHookProjectSettingsFromConfigXml(new File("src/test/resources/project-settings-test-all-states-enabled.xml"));
 		
-		final String htmlStatus = "<span class=\"tcWebHooksMessage\"><a href=\"http://test.server/project.html?projectId=ATestProject\">Test Project</a> :: <a href=\"http://test.server/viewType.html?buildTypeId=TestBuild\">Test Build</a> # <a href=\"http://test.server/viewLog.html?buildTypeId=TestBuild&buildId=123456\"><strong>TestBuild01</strong></a> has <strong>finished</strong> with a status of <a href=\"http://test.server/viewLog.html?buildTypeId=TestBuild&buildId=123456\"> <strong>success</strong></a> and was triggered by <strong>SubVersion</strong></span>";
+		final String htmlStatus = "<span class=\"tcWebHooksMessage\"><a href=\"http://test.server/project.html?projectId=ATestProject\">Test Project</a> :: <a href=\"http://test.server/viewType.html?buildTypeId=TestBuild\">Test Build</a> # <a href=\"http://test.server/viewLog.html?buildTypeId=TestBuild&buildId=123456\"><strong>TestBuild01</strong></a> has <strong>finished</strong> with a status of <a href=\"http://test.server/viewLog.html?buildTypeId=TestBuild&buildId=123456\"> <strong>success</strong></a> and was triggered by <strong>Name</strong></span>";
 		//						   <span class="tcWebHooksMessage"><a href="http://test.server/project.html?projectId=project1">Test Project</a> :: <a href="http://test.server/viewType.html?buildTypeId=bt1">Test Build</a> # <a href="http://test.server/viewLog.html?buildTypeId=bt1&buildId=123456"><strong>TestBuild01</strong></a> has <strong>finished</strong> with a status of <a href="http://test.server/viewLog.html?buildTypeId=bt1&buildId=123456"> <strong>success</strong></a> and was triggered by <strong>SubVersion</strong></span>
 
 		WebHookPayloadContent content = framework.getWebHookContent();
