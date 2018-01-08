b269ea1854907e6a19a33da5987f9359ba268f29
Net Wolf UK <netwolfuk@gmail.com>
2018-01-08 16:51:56 +1300

    Tidyup API Jar cleaner. Add restart link.

--- tcwebhooks-rest-api-legacy/src/main/java/webhook/teamcity/server/rest/web/WebHookRestApiAdminPage.java
+++ tcwebhooks-rest-api-legacy/src/main/java/webhook/teamcity/server/rest/web/WebHookRestApiAdminPage.java
@@ -1,32 +1,17 @@
-/*******************************************************************************
- * Copyright 2017 Net Wolf UK
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
 package webhook.teamcity.server.rest.web;
 
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.jetbrains.annotations.NotNull;
 
 import jetbrains.buildServer.controllers.admin.AdminPage;
+import jetbrains.buildServer.log.Loggers;
+import jetbrains.buildServer.serverSide.SBuildServer;
 import jetbrains.buildServer.serverSide.auth.Permission;
 import jetbrains.buildServer.web.openapi.PagePlaces;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
@@ -39,17 +24,23 @@ public class WebHookRestApiAdminPage extends AdminPage {
 	public static final String TC_WEB_HOOK_REST_API_ADMIN_ID = "tcWebHooksRestApi";
 	private final WebHookTeamCityRestApiZipPluginFixer myPluginFixer;
 
-	public WebHookRestApiAdminPage(@NotNull PagePlaces pagePlaces, 
-								  @NotNull PluginDescriptor descriptor,
-								  @NotNull WebHookTeamCityRestApiZipPluginFixer webHookTeamCityRestApiZipPluginFixer
-								  ) {
+	public WebHookRestApiAdminPage(
+						@NotNull SBuildServer server,
+						@NotNull PagePlaces pagePlaces, 
+						@NotNull PluginDescriptor descriptor,
+						@NotNull WebHookTeamCityRestApiZipPluginFixer webHookTeamCityRestApiZipPluginFixer
+					) {
 		super(pagePlaces);
 		myPluginFixer = webHookTeamCityRestApiZipPluginFixer;
 		setPluginName(TC_WEB_HOOK_REST_API_ADMIN_ID);
 		setIncludeUrl(descriptor.getPluginResourcesPath("WebHookRestApi/adminTab.jsp"));
         addCssFile(descriptor.getPluginResourcesPath("WebHookRestApi/css/tcWebHooksApi.css"));
         addJsFile(descriptor.getPluginResourcesPath("WebHookRestApi/restApiHealthStatus.js"));
-        addJsFile("/js/bs/serverRestart.js");
+        if (isRestartable(server)) {
+        	addJsFile("/js/bs/serverRestart.js");
+        } else {
+        	addJsFile(descriptor.getPluginResourcesPath("WebHookRestApi/serverNoRestart.js"));
+        }
 		setTabTitle("WebHooks REST API");
 		setPosition(PositionConstraint.after("clouds", "email", "jabber", "plugins", "tcWebHooks"));
 		register();
@@ -68,33 +59,14 @@ public class WebHookRestApiAdminPage extends AdminPage {
 	@Override
 	public void fillModel(Map<String, Object> model, HttpServletRequest request) {
 		Map<String, PluginResultBean> fileResults = new TreeMap<>();
-		Set<Path> allPaths = new TreeSet<>();
 		
 		for (Entry<Path, JarReport> e : myPluginFixer.getJarReports().entrySet()) {
 			updatePluginBean(fileResults, e.getKey(), e.getValue());
 		}
-//		allPaths.addAll(myPluginFixer.getFoundApiZipFilesContainingJaxbJars());
-//		allPaths.addAll(myPluginFixer.getFoundUnpackedApiZipFilesContainingJaxbJars());
-//		allPaths.addAll(myPluginFixer.getFoundApiZipFilesNotContainingJaxbJars());
-//		allPaths.addAll(myPluginFixer.getFoundUnpackedApiZipFilesNotContainingJaxbJars());
-//		for (Path p : allPaths) {
-//			updatePluginBean(fileResults, p, 
-//					! myPluginFixer.getFoundApiZipFilesNotContainingJaxbJars().contains(p),
-//					! myPluginFixer.getFoundUnpackedApiZipFilesNotContainingJaxbJars().contains(p));
-//		}
-//		for (Path p : allPaths) {
-//			updatePluginBean(fileResults, p, 
-//					myPluginFixer.getFoundApiZipFilesContainingJaxbJars().contains(p),
-//					myPluginFixer.getFoundUnpackedApiZipFilesContainingJaxbJars().contains(p));
-//		}
 		
 		model.put("hasFoundIssues", myPluginFixer.foundApiZipFilesContainingJaxbJars());
 		model.put("fileResults", fileResults);
 		model.put("restartRequired", myPluginFixer.isHaveFilesBeenCleanedSinceBoot());
-//		model.put("apiZipFilesContainingJars", myPluginFixer.getFoundApiZipFilesContainingJaxbJars());
-//		model.put("unpackedApiFilesContainingJars", myPluginFixer.getFoundUnpackedApiZipFilesContainingJaxbJars());
-//		model.put("apiZipFilesNotContainingJars", myPluginFixer.getFoundApiZipFilesNotContainingJaxbJars());
-//		model.put("unpackedApiFilesNotContainingJars", myPluginFixer.getFoundUnpackedApiZipFilesNotContainingJaxbJars());
 	}
 	
 	private void updatePluginBean(Map<String, PluginResultBean> resultsMap, Path path, JarReport jarReport) {
@@ -111,6 +83,15 @@ public class WebHookRestApiAdminPage extends AdminPage {
 		public int getFileListSize() {
 			return jarReport.getJarsInZipFile().size() + jarReport.getJarsInUnpackedLocation().size();
 		}
+	}
+
+	private boolean isRestartable(SBuildServer server) {
+		Loggers.SERVER.debug("WebHookRestApiAdminPage :: Server Major Version is: " + server.getServerMajorVersion());
+		Loggers.SERVER.debug("WebHookRestApiAdminPage :: Server Minor Version is: " + server.getServerMinorVersion());
+		Loggers.SERVER.debug("WebHookRestApiAdminPage :: Server Version is: " + server.getFullServerVersion());
+		boolean isRestartable = (server.getServerMajorVersion() == 17 && server.getServerMinorVersion() >= 2) || server.getServerMajorVersion() > 17;
+		Loggers.SERVER.info("Server is restartable: " + isRestartable);
+		return isRestartable;
 	}
 
 }
---
+++ tcwebhooks-rest-api-legacy/src/main/resources/buildServerResources/WebHookRestApi/serverNoRestart.js
@@ -0,0 +1,7 @@
+BS.ServerRestarter = {
+
+  restartServer: function () {
+    alert("Sorry. Restarting is only supported since TeamCity 2017.2")
+    return false;
+  }
+};
