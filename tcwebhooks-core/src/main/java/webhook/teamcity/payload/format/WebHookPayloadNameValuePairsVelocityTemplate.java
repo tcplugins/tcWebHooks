79e0c99eda6eee214996b290d16d02a85b351059
Net Wolf UK <netwolfuk@gmail.com>
2018-12-11 16:36:31 +1300

    Add NVPairs template based on Velocity template engine

---
+++ tcwebhooks-core/src/main/java/webhook/teamcity/payload/format/WebHookPayloadNameValuePairsVelocityTemplate.java
@@ -0,0 +1,37 @@
+package webhook.teamcity.payload.format;
+
+import webhook.teamcity.payload.PayloadTemplateEngineType;
+import webhook.teamcity.payload.WebHookContentObjectSerialiser;
+import webhook.teamcity.payload.WebHookPayload;
+import webhook.teamcity.payload.WebHookPayloadManager;
+import webhook.teamcity.payload.variableresolver.WebHookVariableResolverManager;
+
+public class WebHookPayloadNameValuePairsVelocityTemplate extends WebHookPayloadNameValuePairsTemplate implements WebHookPayload, WebHookContentObjectSerialiser {
+	
+	public static final String FORMAT_SHORT_NAME = "nvpairsVelocityTemplate";
+	
+	public WebHookPayloadNameValuePairsVelocityTemplate(WebHookPayloadManager manager, WebHookVariableResolverManager variableResolverManager) {
+		super(manager, variableResolverManager);
+	}
+
+	@Override
+	public String getFormatShortName() {
+		return FORMAT_SHORT_NAME;
+	}
+	
+	@Override
+	public String getFormatDescription() {
+		return "Name Value Pairs - urlencoded  (Velocity template)";
+	}
+
+	@Override
+	public String getFormatToolTipText() {
+		return "Send a x-www-form-urlencoded payload with content from a Velocity template";
+	}
+	
+	@Override
+	public PayloadTemplateEngineType getTemplateEngineType() {
+		return PayloadTemplateEngineType.VELOCITY;
+	}
+	
+}
