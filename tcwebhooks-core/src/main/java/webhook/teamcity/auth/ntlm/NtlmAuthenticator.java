b70839b1af3b919bdd06f2f0f0b3572bca2a45b0
Net Wolf UK <netwolfuk@gmail.com>
2020-05-30 21:04:07 +1200

    Add support for NTLM Authentication

---
+++ tcwebhooks-core/src/main/java/webhook/teamcity/auth/ntlm/NtlmAuthenticator.java
@@ -0,0 +1,53 @@
+package webhook.teamcity.auth.ntlm;
+
+import java.net.URI;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.NTLMScheme;
+import org.apache.http.impl.client.BasicAuthCache;
+
+import webhook.teamcity.auth.AbstractWebHookAuthenticator;
+import webhook.teamcity.auth.WebHookAuthenticator;
+
+public class NtlmAuthenticator extends AbstractWebHookAuthenticator implements WebHookAuthenticator {
+
+		public static final String KEY_PASS = "password";
+		public static final String KEY_USERNAME = "username";
+		public static final String KEY_WORKSTATION = "workstation";
+		public static final String KEY_DOMAIN = "domain";
+		
+		@Override
+		public void addAuthentication(CredentialsProvider credentialsProvider, HttpClientContext httpClientContext, String url) {
+			if (config.getParameters().containsKey(KEY_USERNAME) && config.getParameters().containsKey(KEY_PASS)){
+					URI uri = URI.create(url);
+					AuthScope scope = new AuthScope(uri.getHost(), uri.getPort());
+					Credentials creds = new NTCredentials(config.getParameters().get(KEY_USERNAME), config.getParameters().get(KEY_PASS), config.getParameters().get(KEY_WORKSTATION), config.getParameters().get(KEY_DOMAIN));
+					credentialsProvider.setCredentials(scope, creds);
+					if (Boolean.TRUE.equals(config.getPreemptive())) {
+				           // Create AuthCache instance
+			            AuthCache authCache = new BasicAuthCache();
+			            // Generate BASIC scheme object and add it to the local
+			            // auth cache
+			            NTLMScheme ntlmAuth = new NTLMScheme();
+			            authCache.put(new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme()), ntlmAuth);
+
+			            // Add AuthCache to the execution context
+			            httpClientContext.setAuthCache(authCache);
+					}
+			}
+		}
+
+		@Override
+		public String getWwwAuthenticateChallengePrefix() {
+			return "ntlm";
+		}
+
+}
+
---
+++ tcwebhooks-core/src/main/java/webhook/teamcity/auth/ntlm/NtlmAuthenticatorFactory.java
@@ -0,0 +1,35 @@
+package webhook.teamcity.auth.ntlm;
+
+import webhook.teamcity.auth.AbstractWebHookAuthenticatorFactory;
+import webhook.teamcity.auth.WebHookAuthenticationParameter;
+import webhook.teamcity.auth.WebHookAuthenticator;
+import webhook.teamcity.auth.WebHookAuthenticatorFactory;
+import webhook.teamcity.auth.WebHookAuthenticatorProvider;
+
+public class NtlmAuthenticatorFactory extends AbstractWebHookAuthenticatorFactory implements WebHookAuthenticatorFactory{
+
+		public NtlmAuthenticatorFactory(WebHookAuthenticatorProvider provider) {
+			super(provider);
+			parameterDefinition.add(new WebHookAuthenticationParameter(NtlmAuthenticator.KEY_USERNAME, REQUIRED, NOT_HIDDEN, "Username", "The user name.  This should not include the domain to authenticate with. For example: 'user' is correct whereas 'DOMAIN&#x5c;user' is not."));
+			parameterDefinition.add(new WebHookAuthenticationParameter(NtlmAuthenticator.KEY_PASS, REQUIRED, HIDDEN, "Password", "The password that the webhook should send to authenticate with the webserver."));
+			parameterDefinition.add(new WebHookAuthenticationParameter(NtlmAuthenticator.KEY_WORKSTATION, REQUIRED, NOT_HIDDEN, "Workstation", "The workstation the authentication request is originating from. Essentially, the computer name for the TeamCity server."));
+			parameterDefinition.add(new WebHookAuthenticationParameter(NtlmAuthenticator.KEY_DOMAIN, REQUIRED, NOT_HIDDEN, "Domain", "The Windows domain to authenticate within."));
+		}
+		
+		@Override
+		public String getName() {
+			return "ntlm";
+		}
+		
+		@Override
+		public String getDescription() {
+			return "NTLM Authentication";
+		}
+
+		@Override
+		public WebHookAuthenticator getAuthenticatorInstance() {
+			return new NtlmAuthenticator();
+		}
+
+}
+
