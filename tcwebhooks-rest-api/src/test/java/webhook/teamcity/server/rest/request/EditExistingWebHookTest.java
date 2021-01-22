46f7de37254b5cfb4f9e22236e81e29feb01f3c4
Net Wolf UK <netwolfuk@gmail.com>
2021-01-22 21:39:17 +1300

    More work on Filters and Filter tests

--- tcwebhooks-rest-api/src/test/java/webhook/teamcity/server/rest/request/EditExistingWebHookTest.java
+++ tcwebhooks-rest-api/src/test/java/webhook/teamcity/server/rest/request/EditExistingWebHookTest.java
@@ -1,5 +1,6 @@
 package webhook.teamcity.server.rest.request;
 
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.LoggingFilter;
 import jetbrains.buildServer.serverSide.settings.ProjectSettingsManager;
@@ -36,9 +37,9 @@ import static webhook.teamcity.server.rest.request.WebHooksRequest.API_WEBHOOKS_
 public class EditExistingWebHookTest extends WebHookAbstractSpringAwareJerseyTest {
 	
 	@Autowired
-	ProjectSettingsManager projectSettingsManager;
+	private ProjectSettingsManager projectSettingsManager;
 	
-	WebResource webResource;
+	private WebResource webResource;
 	
 	@Before 
 	public void setup(){
@@ -61,7 +62,7 @@ public class EditExistingWebHookTest extends WebHookAbstractSpringAwareJerseyTes
     }
     
     @Test
-    public void testUpdateParameters() throws JAXBException, IOException, JDOMException {
+    public void testUpdateParameters() throws IOException, JDOMException {
     	
     	SortedMap<String, String> map = new TreeMap<>();
     	ExtraParameters  extraParameters  = new ExtraParameters(map); 
@@ -97,7 +98,7 @@ public class EditExistingWebHookTest extends WebHookAbstractSpringAwareJerseyTes
     }
     
     @Test
-    public void testDeleteParameter() throws JAXBException, IOException, JDOMException {
+    public void testDeleteParameter() throws IOException, JDOMException {
     	
     	SortedMap<String, String> map = new TreeMap<>();
     	ExtraParameters  extraParameters  = new ExtraParameters(map); 
@@ -128,7 +129,7 @@ public class EditExistingWebHookTest extends WebHookAbstractSpringAwareJerseyTes
     }
     
     @Test
-    public void testDeleteParameters() throws JAXBException, IOException, JDOMException {
+    public void testDeleteParameters() throws IOException, JDOMException {
     	
     	SortedMap<String, String> map = new TreeMap<>();
     	ExtraParameters  extraParameters  = new ExtraParameters(map); 
@@ -159,7 +160,84 @@ public class EditExistingWebHookTest extends WebHookAbstractSpringAwareJerseyTes
     }
     
     @Test
-    public void testUpdateFilters() throws JAXBException, IOException, JDOMException {
+    public void testAddFilter() throws IOException, JDOMException {
+    	
+    	SortedMap<String, String> map = new TreeMap<>();
+    	ExtraParameters  extraParameters  = new ExtraParameters(map); 
+    	
+    	WebHookMockingFramework framework = WebHookMockingFrameworkImpl.create(BuildStateEnum.BUILD_FINISHED, extraParameters);
+    	framework.loadWebHookProjectSettingsFromConfigXml(new File("../tcwebhooks-core/src/test/resources/project-settings-test-all-states-enabled-with-branch-and-bearer-auth.xml"));
+    	
+    	Element webhooksParent = new Element("webhooks");
+    	framework.getWebHookProjectSettings().writeTo(webhooksParent);
+    	projectSettingsManager.readFrom(webhooksParent, "TestProject");
+    	
+    	ProjectWebhooks responseMsg = webResource.path(API_WEBHOOKS_URL + "/TestProject").accept(MediaType.APPLICATION_JSON_TYPE).get(ProjectWebhooks.class);
+    	prettyPrint(responseMsg);
+    	assertEquals(1, (int)responseMsg.getCount());
+
+		ProjectWebhook projectWebhook = responseMsg.getWebhooks().get(0);
+    	
+    	assertNotNull(projectWebhook.getUrl());
+    	assertNotNull(projectWebhook.getAuthentication());
+    	assertNotNull(projectWebhook.getParameters());
+    	assertNotNull(projectWebhook.getFilters());
+    	assertEquals(0, (int)projectWebhook.getFilters().getCount());
+
+		ProjectWebHookFilter newFilter = new ProjectWebHookFilter();
+		newFilter.setEnabled(true);
+		newFilter.setRegex(".*.*");
+		newFilter.setValue("yes");
+		ProjectWebHookFilters filters = projectWebhook.getFilters();
+    	filters.getFilters().add(newFilter);
+		ProjectWebhook updatedResponseMsg = webResource.path(projectWebhook.getHref()).accept(MediaType.APPLICATION_JSON_TYPE).put(ProjectWebhook.class, projectWebhook);
+		prettyPrint(updatedResponseMsg);
+		assertEquals(1, (int)updatedResponseMsg.getFilters().getCount());
+    }
+    
+    @Test
+    public void testAddFilters() throws IOException, JDOMException {
+    	
+    	SortedMap<String, String> map = new TreeMap<>();
+    	ExtraParameters  extraParameters  = new ExtraParameters(map); 
+    	
+    	WebHookMockingFramework framework = WebHookMockingFrameworkImpl.create(BuildStateEnum.BUILD_FINISHED, extraParameters);
+    	framework.loadWebHookProjectSettingsFromConfigXml(new File("../tcwebhooks-core/src/test/resources/project-settings-test-all-states-enabled-with-branch-and-bearer-auth.xml"));
+    	
+    	Element webhooksParent = new Element("webhooks");
+    	framework.getWebHookProjectSettings().writeTo(webhooksParent);
+    	projectSettingsManager.readFrom(webhooksParent, "TestProject");
+    	
+    	ProjectWebhooks responseMsg = webResource.path(API_WEBHOOKS_URL + "/TestProject").accept(MediaType.APPLICATION_JSON_TYPE).get(ProjectWebhooks.class);
+    	prettyPrint(responseMsg);
+    	assertEquals(1, (int)responseMsg.getCount());
+
+		ProjectWebhook projectWebhook = responseMsg.getWebhooks().get(0);
+    	
+    	assertNotNull(projectWebhook.getUrl());
+    	assertNotNull(projectWebhook.getAuthentication());
+    	assertNotNull(projectWebhook.getParameters());
+    	assertNotNull(projectWebhook.getFilters());
+    	assertEquals(0, (int)projectWebhook.getFilters().getCount());
+
+		ProjectWebHookFilter newFilter = new ProjectWebHookFilter();
+		newFilter.setEnabled(true);
+		newFilter.setRegex(".*.*");
+		newFilter.setValue("yes");
+		ProjectWebHookFilter newFilter2 = new ProjectWebHookFilter();
+		newFilter2.setEnabled(true);
+		newFilter2.setRegex(".*.*");
+		newFilter2.setValue("yes");
+		ProjectWebHookFilters filters = projectWebhook.getFilters();
+    	filters.getFilters().add(newFilter);
+    	filters.getFilters().add(newFilter2);
+		ProjectWebhook updatedResponseMsg = webResource.path(projectWebhook.getHref()).accept(MediaType.APPLICATION_JSON_TYPE).put(ProjectWebhook.class, projectWebhook);
+		prettyPrint(updatedResponseMsg);
+		assertEquals(2, (int)updatedResponseMsg.getFilters().getCount());
+    }
+    
+    @Test
+    public void testUpdateFilters() throws IOException, JDOMException {
     	
     	SortedMap<String, String> map = new TreeMap<>();
     	ExtraParameters  extraParameters  = new ExtraParameters(map); 
@@ -194,8 +272,43 @@ public class EditExistingWebHookTest extends WebHookAbstractSpringAwareJerseyTes
 		assertEquals(3, (int)updatedResponseMsg.getFilters().getCount());
     }
     
+    
     @Test
-    public void testDeleteFilter() throws JAXBException, IOException, JDOMException {
+    public void testUpdateFiltersFailsWhenFilterMalformed() throws IOException, JDOMException {
+    	
+    	SortedMap<String, String> map = new TreeMap<>();
+    	ExtraParameters  extraParameters  = new ExtraParameters(map); 
+    	
+    	WebHookMockingFramework framework = WebHookMockingFrameworkImpl.create(BuildStateEnum.BUILD_FINISHED, extraParameters);
+    	framework.loadWebHookProjectSettingsFromConfigXml(new File("../tcwebhooks-core/src/test/resources/project-settings-test-all-states-enabled-with-branchNameAndBuildNameFilter.xml"));
+    	
+    	Element webhooksParent = new Element("webhooks");
+    	framework.getWebHookProjectSettings().writeTo(webhooksParent);
+    	projectSettingsManager.readFrom(webhooksParent, "TestProject");
+    	
+    	ProjectWebhooks responseMsg = webResource.path(API_WEBHOOKS_URL + "/TestProject").accept(MediaType.APPLICATION_JSON_TYPE).get(ProjectWebhooks.class);
+    	prettyPrint(responseMsg);
+    	assertEquals(1, (int)responseMsg.getCount());
+
+		ProjectWebhook projectWebhook = responseMsg.getWebhooks().get(0);
+    	
+    	assertNotNull(projectWebhook.getUrl());
+    	assertNull(projectWebhook.getAuthentication());
+    	assertNotNull(projectWebhook.getParameters());
+    	assertNotNull(projectWebhook.getFilters());
+    	assertEquals(2, (int)projectWebhook.getFilters().getCount());
+
+		ProjectWebHookFilter newFilter = new ProjectWebHookFilter();
+		newFilter.setEnabled(true);
+		newFilter.setRegex(".*.*");
+		ProjectWebHookFilters filters = projectWebhook.getFilters();
+    	filters.getFilters().add(newFilter);
+		ClientResponse updatedResponseMsg = webResource.path(projectWebhook.getHref()).accept(MediaType.APPLICATION_JSON_TYPE).put(ClientResponse.class, projectWebhook);
+		assertEquals(422, updatedResponseMsg.getStatus());
+    }
+    
+    @Test
+    public void testDeleteFilter() throws IOException, JDOMException {
     	
     	SortedMap<String, String> map = new TreeMap<>();
     	ExtraParameters  extraParameters  = new ExtraParameters(map); 
@@ -225,7 +338,7 @@ public class EditExistingWebHookTest extends WebHookAbstractSpringAwareJerseyTes
 		assertEquals(1, (int)updatedResponseMsg.getFilters().getCount());
     }
     @Test
-    public void testDeleteFilters() throws JAXBException, IOException, JDOMException {
+    public void testDeleteFilters() throws IOException, JDOMException {
     	
     	SortedMap<String, String> map = new TreeMap<>();
     	ExtraParameters  extraParameters  = new ExtraParameters(map); 
