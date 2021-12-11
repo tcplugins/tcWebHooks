8c168bd9920bdf79efb7bd6b5393099b22356468
Net Wolf UK <netwolfuk@gmail.com>
2021-12-11 22:48:55 +1300

    Return correct Header object from REST api

--- tcwebhooks-rest-api/src/main/java/webhook/teamcity/server/rest/model/webhook/ProjectWebHookHeader.java
+++ tcwebhooks-rest-api/src/main/java/webhook/teamcity/server/rest/model/webhook/ProjectWebHookHeader.java
@@ -1,15 +1,25 @@
 package webhook.teamcity.server.rest.model.webhook;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
 import lombok.Getter;
 import lombok.Setter;
 import webhook.teamcity.settings.WebHookHeaderConfig;
 
 @Getter @Setter
-public class ProjectWebHookHeader extends WebHookHeaderConfig {
+@XmlAccessorType(XmlAccessType.FIELD)
+
+public class ProjectWebHookHeader {
 	
+	@XmlAttribute
 	private int id;
-	
-	String href;
+	private String name;
+	private String value;
+
+	@XmlAttribute
+	private String href;
 
 	public static ProjectWebHookHeader copy(WebHookHeaderConfig header, Integer count, String href) {
 		ProjectWebHookHeader t = new ProjectWebHookHeader();
