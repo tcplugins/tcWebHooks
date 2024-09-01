fd7c3540d55280c6bc6e550e6da497e6d4a7d154
Net Wolf UK <netwolfuk@gmail.com>
2024-09-01 19:21:30 +1200

    Remove unused import

--- tcwebhooks-core/src/main/java/webhook/teamcity/settings/WebHookSettingsManagerImpl.java
+++ tcwebhooks-core/src/main/java/webhook/teamcity/settings/WebHookSettingsManagerImpl.java
@@ -2,7 +2,6 @@ package webhook.teamcity.settings;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
